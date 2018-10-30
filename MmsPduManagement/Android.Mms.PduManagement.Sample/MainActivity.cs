d11692062a7a61299172c6e7bd7c2f0be29ab700
yasmanillanes <llanesyasmani@gmail.com>
2018-10-30 14:49:55 -0400

    Initial commit

---
+++ MmsPduManagement/Android.Mms.PduManagement.Sample/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Android.Mms.PduManagement.Sample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.SetContentView(Resource.Layout.activity_main);
+        }
+	}
+}
+
